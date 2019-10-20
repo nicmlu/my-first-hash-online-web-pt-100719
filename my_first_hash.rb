2bc484e8d23537a691e6f9f9da4b7dd9f7b2563e
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-20 23:15:25 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  your_hash = { "name" => "Nicole Lucien", "age" => 35, "occupation" => "Future Software Engineer" }
 end
 
 
